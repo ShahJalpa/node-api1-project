4995a1d8bbda277029a2516d1df530daa863388d
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-05 18:26:53 -0400

    get request is working with specific id

--- api/server.js
+++ api/server.js
@@ -35,4 +35,21 @@ server.get("/api/users", (req, res) => {
         })
 })
 
+//Get request with an id
+server.get("/api/users/:id", (req, res) => {
+   const idVar = req.params.id
+    User.findById(idVar)
+        .then(user => {
+            console.log(user)
+            if(!user){
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }else{
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The user information could not be retrieved" })
+        })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
