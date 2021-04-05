b347b4105ce17065c621550fe2bcdbaf04bef82e
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-05 17:54:23 -0400

    retriving all the users

--- api/server.js
+++ api/server.js
@@ -23,4 +23,16 @@ server.post("/api/users", (req, res) => {
     }
 })
 
+//Get request
+server.get("/api/users", (req, res) => {
+    User.find()
+        .then(users => {
+            console.log(users)
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
