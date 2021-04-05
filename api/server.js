6dba0c7e1667a8619744a4150434643119118ae8
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-05 18:59:30 -0400

    put request

--- api/server.js
+++ api/server.js
@@ -67,4 +67,26 @@ server.delete("/api/users/:id", async (req, res) => {
     }
 })
 
+//Put request
+server.put("/api/users/:id", (req, res) => {
+    const {id} = req.params
+    const updateUser = req.body
+
+    if(!updateUser.name || updateUser.bio){
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    }else{
+        User.update(id, updateUser)
+            .then(user => {
+                if(!user){
+                    res.status(404).json({ message: "The user with the specified ID does not exist" })
+                }else{
+                    res.status(200).json(user)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The user information could not be modified" })
+            })
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
